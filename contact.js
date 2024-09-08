62f14d6923426f17a563734cbf141470d605d1aa
abdisae <root@linuxlite-X201EP.(none)>
2024-09-08 23:31:06 +0700

    memperbaiki contact page

--- contact.js
+++ contact.js
@@ -1,15 +1,19 @@
 contact=()=>{
-	const app= document.getElementById('app').innerHTML=`<section class="bg-main pbs-80 align-content-center">
-	<div class="row text-center col-2">
-		<div class="row-col-2">
-		<h1 class="col-span mb-20">Hubungi saya di kontak berikut</h1>
-			<div><a href="http//wa.me">whatsapp</a></div>
-			<div><a href="http//wa.me">whatsapp</a></div>
-			<div><a href="http//wa.me">whatsapp</a></div>
-			<div><a href="http//wa.me">whatsapp</a></div>
-			<div><a href="http//wa.me">whatsapp</a></div>
-			<div><a href="http//wa.me">whatsapp</a></div>
-			<div><a href="http//wa.me">whatsapp</a></div>
+	const app= document.getElementById('app').innerHTML=`<section>
+	<div class="contact">
+		<h1>Hubungi saya di kontak berikut</h1>
+		<div class="container">
+			<div class="wrapper">
+				<div class="link">
+					<a href="http//wa.me">whatsapp</a>
+					<a href="http//wa.me">whatsapp</a>
+					<a href="http//wa.me">whatsapp</a>
+				</div><hr/>
+				<div class="map">
+					<p>This map</p>
+					<img src="image/peta.jpeg" alt="map" >
+				</div>
+			</div>
 		</div>
 	</div>
 </section>`
